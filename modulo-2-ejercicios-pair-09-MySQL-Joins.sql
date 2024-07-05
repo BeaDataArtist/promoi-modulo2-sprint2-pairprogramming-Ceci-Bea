6a938d96216de6326dc1e0fd253e64eb17caee34
Cecilia Singh Caro <ceciliasinghcaro@gmail.com>
2024-07-05 21:45:00 +0200

    subo hasta ejercicio 2

--- modulo-2-ejercicios-pair-09-MySQL-Joins.sql
+++ modulo-2-ejercicios-pair-09-MySQL-Joins.sql
@@ -3,13 +3,28 @@ USE northwind;
 -- 1. Pedidos por empresa en UK:
 	-- Desde las oficinas en UK nos han pedido con urgencia que realicemos una consulta a la base de datos con la que podamos conocer cuántos pedidos ha realizado cada empresa cliente de UK. 
 	-- Nos piden el ID del cliente y el nombre de la empresa y el número de pedidos.
+    SELECT c.customer_id, c.company_name, COUNT(o.order_id) AS numpedido
+		FROM customers AS c
+		INNER JOIN orders AS o ON c.customer_id = o.customer_id
+        WHERE c.country = "UK"
+        GROUP BY c.customer_id, c.company_name;
+      
+		    
     
 -- 2. Productos pedidos por empresa en UK por año:
 	-- Desde Reino Unido se quedaron muy contentas con nuestra rápida respuesta a su petición anterior y han decidido pedirnos una serie de consultas adicionales. 
     -- La primera de ellas consiste en una query que nos sirva para conocer cuántos objetos ha pedido cada empresa cliente de UK durante cada año.
     -- Nos piden concretamente conocer el nombre de la empresa, el año, y la cantidad de objetos que han pedido. Para ello hará falta hacer 2 joins.
+    SELECT c.company_name, YEAR(o.order_date) AS fecha, SUM(od.quantity) AS total
+		FROM customers AS c
+        INNER JOIN orders AS o ON c.customer_id = o.customer_id
+        INNER JOIN order_details AS od ON o.customer_id = od.order_id
+		WHERE c.country = "UK"
+        GROUP BY c.customer_id, c.company_name
     
--- 3. Mejorad la query anterior:
+    
+    
+    -- 3. Mejorad la query anterior:
 	-- Lo siguiente que nos han pedido es la misma consulta anterior pero con la adición de la cantidad de dinero que han pedido por esa cantidad de objetos, teniendo en cuenta los descuentos, etc. 
     -- Ojo que los descuentos en nuestra tabla nos salen en porcentajes, 15% nos sale como 0.15.
     
