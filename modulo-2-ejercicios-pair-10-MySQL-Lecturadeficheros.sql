b3126872d73d2081f07c2179f969152345826a02
Cecilia Singh Caro <ceciliasinghcaro@gmail.com>
2024-07-08 10:35:20 +0200

    subo archivo de ejericio lectura de ficheros

---
+++ modulo-2-ejercicios-pair-10-MySQL-Lecturadeficheros.sql
@@ -0,0 +1,21 @@
+
+-- Pair programming Lectura de ficheros
+-- Enunciado:
+-- En este pair vamos a trabajar la apertura y exploración de archivos. Para finalmente guardar el archivo con un formato diferente.
+
+
+
+
+-- Ejercicio 1: Hacer las importaciones necesarias para abrir un archivo de tipo "csv" en Pandas.
+
+-- Ejercicio 2:Abrir el archivo, creando un Dataframe. Aseguraros de que no incluye la columna "Unnamed: 0".
+
+-- Ejercicio 3:Guardar el Dataframe en formato Excel y después crear un Dataframe con el Excel. ¿EL Dataframe es similar al que teníais antes?
+
+-- Ejercicio 4:Crear una carpeta llamada Files y guardar el mismo archivo en esa carpeta en formato csv .
+
+-- Ejercicio 5:Copia el archivo personal.json en la carpeta Files.
+
+-- Ejercicio 6:Abre el json y haz las modificaciones necesarias para poder abrirlo correctamente en Pandas.
+
+-- Ejercicio 7:Guardar el json modificado y abrirlo en Pandas para que tenga formato de tabla.
